27eb9b75137384c9b00edb795eb85990c94f2d6b
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-09-13 04:03:56 +0530

    assignment -3 done

--- 4.Assignment-3/ques.js
+++ 4.Assignment-3/ques.js
@@ -14,7 +14,7 @@ function findHashWithPrefix(prefix) {
     let input = 0;
 
     while (true) {
-        let inputString = "harkirat => Raman | Rs 100" + input.toString();
+        let inputString = "Bhupesh => New | Rs 100" + input.toString();
         let hash = crypto.createHash('sha256').update(inputString).digest('hex');
         if (hash.startsWith(prefix)) {
             return { input: inputString, hash: hash };
