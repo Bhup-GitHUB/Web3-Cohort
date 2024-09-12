7b7e380604f4b978875626f2d8c580d5f76c39bc
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-09-13 01:33:37 +0530

    assignment 3 done

---
+++ 4.Assignment-3/ques.js
@@ -0,0 +1,37 @@
+
+// Assignment #3
+// What if I ask you to find a nonce for the following input - 
+
+// harkirat => Raman | Rs 100
+// Ram => Ankit | Rs 10
+
+
+
+
+const crypto = require('crypto');
+
+function findHashWithPrefix(prefix) {
+    let input = 0;
+
+    while (true) {
+        let inputString = "harkirat => Raman | Rs 100" + input.toString();
+        let hash = crypto.createHash('sha256').update(inputString).digest('hex');
+        if (hash.startsWith(prefix)) {
+            return { input: inputString, hash: hash };
+        }
+        input++;
+    }
+}
+
+const result = findHashWithPrefix("00000");
+console.log("Input: " + result.input);
+console.log("Hash: " + result.hash);
+
+
+
+
+
+
+
+
+
